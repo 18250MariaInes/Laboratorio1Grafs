1f28ee95d9f45566e403af7620dc3073f632117c
Maria Vasquez <maria.ines.vf@gmail.com>
2020-07-12 12:48:08 -0600

    documentacion corregida

--- Engine3D.py
+++ Engine3D.py
@@ -15,12 +15,13 @@ height=1080
 posX = width/4
 posY = height/4
 #creacion de Window
+#los ultimos 3 parametros es para settear el color del fondo de la ventana con parametros de 0-1 para rojo, verde y azul
 r = Render(width,height,0,0,0)
 
 #creacion del viewport
 r.glViewPort(posX, posY, width - width/2 , height - height/2)
 
-#cambio de color con el que se hará el punto
+#cambio de color con el que se hará el punto con parametros de 0-1 para r, g, b
 r.glColor(1,1,1)
 
 #se dibuja el punto en relación al viewport
